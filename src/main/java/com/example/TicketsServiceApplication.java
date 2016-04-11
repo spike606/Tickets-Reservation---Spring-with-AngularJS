14af7c9213ff90b52cb0b9819c71f5422495c795
spike606 <krystusw@gmail.com>
2016-04-11 19:35:19 +0200

    initial commit
    Projekt oparty o spring boot. Dodane zależności webowe, mail, postgres,
    jpa, spring security, validation, bower do zarzadzania
    frameworkami frontend - w pliku bower.json.

---
+++ src/main/java/com/example/TicketsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsServiceApplication.class, args);
+	}
+}
